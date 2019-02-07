8cd13018e40622daedbc7d4de548696e1b9ff77b
Ian Maddison <ian@i.need.stronger.coffee>
2019-02-07 18:06:37 +0000

    Refactor a lot of stuff to tidy up and fix the protocol. Adding publicKey ids to published patterns

---
+++ src/main/java/com/goodforgoodbusiness/shared/Combinatorics.java
@@ -0,0 +1,56 @@
+package com.goodforgoodbusiness.shared;
+
+import java.util.Deque;
+import java.util.List;
+
+public class Combinatorics {
+	public static void main(String [] args) {
+		// pick 1
+		
+		// (1 _ _ _)
+		// (_ 2 _ _)
+		// (_ _ 3 _)
+		// (_ _ _ 4)
+		
+		// pick 2
+		
+		var input = new int [] { 1, 2, 3, 4 };
+		
+		// (1 2 _ _)
+		// (1 _ 3 _)
+		// (1 _ _ 4)
+		
+		// (_ 2 3 _)
+		// (_ 2 _ 4)
+		
+		// (_ _ 3 4)
+		
+		for (int x = 0; x < input.length; x++) {
+			for (int y = x + 1; y < input.length; y++) {
+				for (int a = 0; a < input.length; a++) {
+					if (a == x || a == y) {
+						System.out.print(input[a] + " ");
+					}
+					else {
+						System.out.print("  ");
+					}
+				}
+				
+				System.out.println();
+			}
+		}
+
+	}
+	
+	private static void round(int start, int count, int [] input, Deque<Integer> picked, List<Deque<Integer>> result) {
+		for (int x = start; x < input.length; x++) {
+			picked.addLast(input[x]);
+			if (picked.size() == count) {
+				result.add(picked);
+			}
+			else {
+				round(start + 1, count, input, picked, result);
+			}
+		}
+	}
+}
--- src/main/java/com/goodforgoodbusiness/shared/TripleUtil.java
+++ src/main/java/com/goodforgoodbusiness/shared/TripleUtil.java
@@ -2,36 +2,11 @@ package com.goodforgoodbusiness.shared;
 
 import static org.apache.jena.graph.Node.ANY;
 
-import java.util.Optional;
-
 import org.apache.jena.graph.Node;
 import org.apache.jena.graph.Triple;
 
 public class TripleUtil {
 	public static final Triple ANYANYANY = new Triple(ANY, ANY, ANY);
-	
-	public static Optional<String> valueOf(Node node) {
-		if (node == null || node.equals(Node.ANY)) {
-			return Optional.empty();
-		}
-		else if (node.isURI()) {
-			return Optional.of(node.getURI());
-		}
-		else if (node.isLiteral()) {
-			return Optional.of(node.getLiteralValue().toString());
-		}
-		else {
-			throw new IllegalArgumentException(node.toString());
-		}
-	}
-	
-	public static String [] toValueArray(Triple triple) {
-		return new String [] {
-			valueOf(triple.getSubject()).orElse(null),
-			valueOf(triple.getPredicate()).orElse(null),
-			valueOf(triple.getObject()).orElse(null)
-		};
-	}
 	
 	public static boolean isConcrete(Triple triple) {
 		if (triple.getSubject() == null || triple.getSubject().equals(ANY)) {
@@ -60,36 +35,5 @@ public class TripleUtil {
 		}
 		
 		return false;
-	}
-	
-	/**
-	 * Create all possible pattern triples that match a concrete Triple
-	 */
-	public static Triple [] makePatterns(Triple t) {
-		if (!isConcrete(t)) {
-			throw new IllegalArgumentException("Triple must be concrete");
-		}
-		
-		var sub = t.getSubject();
-		var pre = t.getPredicate();
-		var obj = t.getObject();
-		
-		return new Triple[] {
-			// pick 3
-			new Triple(sub, pre, obj),
-			
-			// pick 2
-			new Triple(sub, pre, ANY),
-			new Triple(sub, ANY, obj),
-			new Triple(ANY, pre, obj),
-			
-			// pick 1
-			new Triple(sub, ANY, ANY),
-			new Triple(ANY, pre, ANY),
-			new Triple(ANY, ANY, obj),
-			
-			// pick 0
-			new Triple(ANY, ANY, ANY),
-		};
 	}
 }
