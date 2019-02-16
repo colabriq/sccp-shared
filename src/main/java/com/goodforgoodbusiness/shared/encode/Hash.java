dcac42de8beff1faf313b3bdf1ca738d65882198
Ian Maddison <ian@i.need.stronger.coffee>
2019-02-16 14:03:21 +0000

    Enable convergent encryption

--- src/main/java/com/goodforgoodbusiness/shared/encode/Hash.java
+++ src/main/java/com/goodforgoodbusiness/shared/encode/Hash.java
@@ -4,14 +4,28 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class Hash {
-	private static final String HASH_ALGORITHM = "SHA-256";
+	private static final String HASH_ALGORITHM = "SHA-512";
 	
-	public static byte [] sha256(byte [] input) {
+	public static byte [] sha512(byte [] input) {
 		try {
 			return MessageDigest.getInstance(HASH_ALGORITHM).digest( input );
 		}
 		catch (NoSuchAlgorithmException e) {
 			throw new RuntimeException(HASH_ALGORITHM + " not supported", e);
 		}
+	}
+	
+	/**
+	 * Truncate the hash results for various purposes.
+	 * Not hard, but encapsulate to stay consistent.
+	 */
+	public static byte [] truncate(byte [] input, int length) {
+		if (length > input.length) {
+			throw new IllegalArgumentException("Requested length longer than input length: " + length);
+		}
+		
+		var result = new byte[length];
+		System.arraycopy(input, 0, input, 0, result.length);
+		return result;
 	}
 }
