b260a513804cfa59c9186b6bfca488b3e999470c
Ian Maddison <ian@i.need.stronger.coffee>
2019-07-23 18:49:13 +0100

    Add a safe stop method

--- src/main/java/com/goodforgoodbusiness/shared/executor/PrioritizedExecutor.java
+++ src/main/java/com/goodforgoodbusiness/shared/executor/PrioritizedExecutor.java
@@ -4,11 +4,13 @@ import static com.goodforgoodbusiness.shared.executor.PrioritizedTask.Priority.N
 import static java.util.concurrent.TimeUnit.MINUTES;
 
 import java.util.concurrent.CancellationException;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import java.util.concurrent.PriorityBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
 
 import org.apache.log4j.Logger;
 
@@ -30,6 +32,10 @@ public class PrioritizedExecutor extends ThreadPoolExecutor {
 	
 	private static final int INITIAL_CAPACITY = 10;
 	
+	// record runtimes for tasks
+	private final ConcurrentHashMap<Class<? extends Runnable>, AtomicLong> totalTaskTimings = new ConcurrentHashMap<>();
+	private final ThreadLocal<Long> currentTaskStarted = new ThreadLocal<>();
+	
 	public PrioritizedExecutor( int poolSize) {
 		// use a priority queue to decide the order of task execution
 		super(
@@ -46,12 +52,21 @@ public class PrioritizedExecutor extends ThreadPoolExecutor {
 	@Override
 	protected void beforeExecute(Thread t, Runnable r) {
 		log.info("ExecutorService queue size = " + getQueue().size());
+		currentTaskStarted.set(System.currentTimeMillis());
 		super.beforeExecute(t, r);
 	}
 	
 	@Override
 	protected void afterExecute(Runnable r, Throwable t) {
 		super.afterExecute(r, t);
+		
+		if (currentTaskStarted.get() != null) {
+			long elapsed = System.currentTimeMillis() - currentTaskStarted.get();
+			currentTaskStarted.remove();
+			
+			totalTaskTimings.computeIfAbsent(r.getClass(), c -> new AtomicLong(0));
+			totalTaskTimings.get(r.getClass()).addAndGet(elapsed);
+		}
 		
 		var cause = t;
         if (t == null && r instanceof Future<?>) {
